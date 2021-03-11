21e9843d855d85af87fd4dd02438cc48a9fa352f
Sander Arjan Benoist <sabenoist5@gmail.com>
2021-03-11 17:32:35 +0100

    implemented button to activate meditation session

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    public Material buttonInactive;
+    public Material buttonActive;
+
+    // the player touches the button
+    void OnTriggerEnter(Collider col) {
+        GetComponent<MeshRenderer>().material = buttonActive;
+    }
+
+    // the player lets go of the button
+    void OnTriggerExit(Collider col) {
+        GetComponent<MeshRenderer>().material = buttonInactive;
+    }
+
+
+}
