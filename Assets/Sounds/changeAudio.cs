91a1f0e24c4ab191c2867440b260b42acbae7d81
miyu0201 <y.minghui@yahoo.com>
2021-03-12 09:57:12 +0100

    editing audio

--- Assets/Sounds/changeAudio.cs
+++ Assets/Sounds/changeAudio.cs
@@ -6,13 +6,22 @@ using UnityEngine;
 public class changeAudio : MonoBehaviour
 {
     public AudioClip otherClip;
+    public float delayTime;
+    public float dayTime;
+    //public GameObject dayNightCycle;
+
+
 
     IEnumerator Start()
     {
         AudioSource audio = GetComponent<AudioSource>();
 
+        // dayNightCycle = 
+       // dayTime = GetComponent<timeOfDay>();
+       // delayTime = dayTime / 2 - 5F;
         audio.Play();
         yield return new WaitForSeconds(145F);
+      //  yield return new WaitForSeconds(delayTime);
         audio.clip = otherClip;
         audio.volume = 1f;
         audio.Play();
